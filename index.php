5c5a28b097102ebd0423fe82e420d8eae21e5db9
Jenkins <jenkins@ip-172-31-84-96.ec2.internal>
2019-09-10 03:31:57 +0000

    test

--- index.php
+++ index.php
@@ -9,9 +9,13 @@
 		include_once('assets/includes/header.php');
 ?>
 <!-- START PAGE CONTENT -->
+<<<<<<< HEAD
 i<h1> ram branch page <h1>
 
 <h1> Dev branch <h1>
+=======
+<h1> ram branch <h1>
+>>>>>>> parent of ea63a21... first change
 <p>
 You’ll find EWU in Chicago’s South Loop neighborhood, easily accessible by bus, train and subway. Your professors are experienced professionals, experts in their fields with the ability and willingness to provide the guidance you need to succeed. You’ll also find a financial aid department dedicated to helping you – through part-time employment, grants, scholarships, loans and more – pursue every available means to attend.
 <br/><br/>
